859b75259c8f52912a88e7aaa9393bd31f925969
Conor Beamish <conorbeamish@hotmail.co.uk>
2024-05-07 16:08:09 +0100

    7 - Add a total for pricing

--- client/src/pages/ShoppingList.jsx
+++ client/src/pages/ShoppingList.jsx
@@ -1,29 +1,38 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import UserContext from '../contexts/User'
 import { baseURL } from '../utils/baseUrl';
 import ItemList from "../components/ItemList"
 import axios from 'axios';
 
 const ShoppingList = () => {
-    const {userData, setUserData} = useContext(UserContext);
-    
+    const { userData, setUserData } = useContext(UserContext);
+    const [totalPrice, setTotalPrice] = useState(0);
+
     useEffect(() => {
         axios({
             method: "get",
             url: `${baseURL()}api/users/${userData.id}/items`,
-          }).then(res =>{
-            const items = res.data
-            setUserData({...userData, items })
-          })
-          .catch(err => console.log(err))
-      }, [ ])
-      
+        })
+        .then(res => {
+            const items = res.data;
+            setUserData({ ...userData, items });
+            calculateTotalPrice(items); // Calculate total price when items are fetched
+        })
+        .catch(err => console.log(err));
+    }, [userData.id, setUserData]);
+
+    const calculateTotalPrice = (items) => {
+        const total = items.reduce((acc, item) => acc + item.price, 0);
+        setTotalPrice(total);
+    }
+
     return (
         <>
             <h3>Your shopping list</h3>
-            <ItemList items={userData.items}/>
+            <ItemList items={userData.items} />
+            <p>Total: £{totalPrice.toFixed(2)}</p> {/* Display total price */}
         </>
-    )
+    );
 }
 
-export default ShoppingList
+export default ShoppingList;
